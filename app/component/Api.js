8eb6cf37dbdc02ae9947acdb63c3ac47ca1faf6e
UMANG-2003 <umangkumar20003@gmail.com>
2025-03-22 16:13:08 +0530

    #

--- app/component/Api.js
+++ app/component/Api.js
@@ -39,7 +39,7 @@ export default function Home() {
           value={query}
           onChange={(e) => setQuery(e.target.value)}
           placeholder="Search for a song..."
-          style={{ padding: "10px", marginRight: "10px", width: "230px" }}
+          style={{ padding: "10px", marginRight: "10px", width: "210px" }}
           className="bg-gray-500 rounded-3xl"
         />
         <button type="submit" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium  text-sm px-5 py-2.5 text-center me-2 mb-2 rounded-3xl">Search</button>
